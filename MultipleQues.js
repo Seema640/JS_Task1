8d5186c087687fe46384a43a1ce3ff59ce4febbd
Seema Tamang <seema.tamang640@gmail.com>
2019-01-10 22:19:41 +0545

    Add files via upload

--- MultipleQues.js
+++ MultipleQues.js
@@ -1,77 +1,93 @@
+var correct = 0 , ans_1 = "A", ans_2 = "B", ans_3 = "B", userchoice;
 
-window.addEventListener("load", renderQuestion, false);
+    //to record the click on the radio
+    $(":radio").click(function(){
 
-var pos = 0, x , test, test_status, store=0 , question, questions, choice, choices, chA, chB, chC, correct = 0;
+        var radioName = $(this).attr("name"); //this line gets the radio name i.e. in this case: name
 
-var Questions = 
-[
-    [" ______ is the biggest planet in the solar system.", "Uranus" , "Jupiter" , "Mars" , "B"],
-    ["How many colors are there in a rainbow?","seven","six","two","A"],
-    ["What is the height of Mt.Everest?","6789m","8848m","87489m","B"]
-];
+        $(":radio[name='"+radioName+"']:not(:checked)").attr("disabled", true); //disables the unchecked radios with the same name: choice
 
-function take(x)
-{
-    return document.getElementById(x);
-}
+    });
 
-function renderQuestion() {
-    test = take("test");
-   
-    if(pos >= Questions.length)
+    //records the id of submit button
+    $(":submit").click(function() {
+        $(":radio[name = 'choice']").attr("disabled", false); //enables all the buttons for next question.
+
+    });
+
+    //call of  first function after click on submit of first question.
+    function submit() {
+    
+    if(document.getElementById('phasetwo') !== undefined)     //extracts and checks the data of second question
     {
-        test.innerHTML = "<h2>You got" + correct + " of " + Questions.length +"questions correct</h2>";
-        ("test_status").innerHTML = "Test Completed";
-		pos = 0;
-		correct = 0;
-        return false;
-            
+        document.getElementById ('phasetwo').style.display = "block"; //displays the second set of questions
+        document.getElementById ('phaseone').style.display = "none"; //hides the first set of questions.
     }
 
-    ("test_status").innerHTML = "Question " + (pos) + " of " + Questions.length;
-        question = Questions[pos] [0];
-        chA = Questions[pos] [1];
-        chB = Questions[pos] [2];
-        chC = Questions[pos] [3];
-      
-        test.innerHTML = "<h3>"+question+"</h3>";
 
-        test.innerHTML += "<input type = 'radio' name = 'choice' value='A'> " + chA + "<br>";
-        test.innerHTML += "<input type = 'radio' name = 'choice' value='B'> " + chB + "<br>";
-        test.innerHTML += "<input type = 'radio' name = 'choice' value='C'> " + chC + "<br>";
-        test.innerHTML += "<button onclick = 'submit()'>Submit Answer</button>";
+    $(document).ready(function() {
+        
+        userchoice = document.querySelector('input[class="option"]:checked').value;  //stores the value of the radio selected by user
+        if(userchoice == ans_1)            //if the users value and stored answer is correct, correct is incremented.
+        {
+            correct++;
+            console.log(correct);
+     
+        }
+     
+     }); 
+
+    }
+
+function second_que() {
+
+    if(document.getElementById('phasethree') !== undefined)
+    {
+        document.getElementById('phasethree').style.display = "block";
+        document.getElementById('phasetwo').style.display = "none";
+    }
+ 
+     $(document).ready(function() {
+ 
+        userchoice = document.querySelector('input[class="option"]:checked').value;
+        if(userchoice == ans_2)
+        {
+            correct++;
+            console.log(correct);
+      
+        }
+              
+      }); 
     
 }
 
-function submit()
+
+function third_que() {
+
+    if(document.getElementById('phasefour') !== undefined)
     {
-        extract = document.getElementsByName("choice");  //extract is an array and stores the choices options.
-   
-        len = extract.length; //len stores length of extract
+        document.getElementById('phasefour').style.display = "block";
+        document.getElementById('phasethree').style.display = "none";
 
-        var i; 
-   
-        var checked = false;  //Initially checked is assigned false
-          
-        for (i = 0 ; i < len ; i++) //for loop runs till the end of array and selects every element.
-            {
-                if( extract[i].checked) // checks if the element of the array is selected by user.
-                {
-                   extract[i].checked = false;  // deselects the answer.
-                    store = extract[i].value;
-                }
-                if( store == Questions[pos] [4])
-                {
-                    correct++;
-                    break;
-                }
+    }
+    
+     $(document).ready(function() {
+        
+    
+        userchoice = document.querySelector('input[class="option"]:checked').value;
+        if(userchoice == ans_3)
+        {
+            correct++;
+            console.log(correct);
+      
+        } 
+        var x = document.getElementById("demo").innerHTML;    //extracts data from paragraph through its id and stores in variable x. 
+        document.getElementById("demo").innerHTML = ("<b>You made " +correct + " " + "answers correct out of 3 questions</b>");
+        //String displayed using innerHTML    
 
-               
-            }
-            pos++;
-            renderQuestion();
 
-    }  
+    });
+       
+}
 
-           
-                
+
